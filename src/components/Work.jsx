3755a190d7a535d06bf48762dd2ce05bf9052c9b
Garrett Clark <clarkg@berea.edu>
2023-09-12 08:33:41 -0400

    Added blog link in work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,8 @@ import Eco from "../assets/ecoB4C.jpg";
 import WHD from "../assets/WHDparser.PNG";
 import Mold from "../assets/mold.png";
 import StreetAssist from "../assets/StreetAssist.png";
+import Blogger from "../assets/blogger.png";
+
 
 const Work = () => {
   return (
@@ -17,6 +19,23 @@ const Work = () => {
           <p className="py-6">// Check out some of my recent work</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div
+            style={{ backgroundImage: `url(${Blogger})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                CELTS
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://www.blogger.com/blog/posts/6008878360572385628">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    CSC 426 Blog
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${bereaCollege})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
